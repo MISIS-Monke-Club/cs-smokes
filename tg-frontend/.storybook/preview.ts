897003bc6256219293a9702c0bf1e597783024d7
Artemitol <artemiy.averin@gmail.com>
2025-03-13 20:51:58 +0300

    initialisated storybook

---
+++ tg-frontend/.storybook/preview.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-default-export */
+import type { Preview } from "@storybook/react"
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+}
+
+export default preview
