8e37334c7310b2e82c6f9c396fc801013e852334
Artemitol <artemiy.averin@gmail.com>
2025-03-31 16:10:17 +0300

    updated decorator for storybook

--- tg-frontend/.storybook/preview.tsx
+++ tg-frontend/.storybook/preview.tsx
@@ -26,11 +26,11 @@ const preview: Preview = {
     },
     decorators: [
         (Story) => (
-            <MemoryRouter initialEntries={["/"]}>
-                <ThemeProvider>
+            <ThemeProvider>
+                <MemoryRouter initialEntries={["/"]}>
                     <Story />
-                </ThemeProvider>
-            </MemoryRouter>
+                </MemoryRouter>
+            </ThemeProvider>
         ),
     ],
     tags: ["autodocs"],
