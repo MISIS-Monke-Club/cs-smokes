e9468c4df7834427632031c06cdb818ef36c7e9e
Artemitol <artemiy.averin@gmail.com>
2025-04-02 16:03:19 +0300

    created @features/edit-profile in tg-front, refactored form and logic to the features

--- tg-frontend/src/pages/edit-profile-page/edit-profile-page.tsx
+++ tg-frontend/src/pages/edit-profile-page/edit-profile-page.tsx
@@ -1,24 +1,13 @@
 import classes from "./edit-profile-page.module.scss"
-import { EditProfileForm } from "@widgets/edit-profile-form"
-import { useEditProfile } from "@features/edit-profile"
+import { GoBack } from "@features/go-back"
+import { EditProfileForm } from "@features/edit-profile"
 
 export function EditProfilePage() {
-    const { user, isLoading, updateUser } = useEditProfile()
-
-    if (isLoading) {
-        return <b>Loading...</b>
-    }
-
-    if (!user) {
-        return <b>Профиль не найден</b>
-    }
-
     return (
-        <>
-            <div className={classes.wrapper}>
-                <h1 className={classes.title}>Редактирование профиля</h1>
-                <EditProfileForm user={user} onSubmit={updateUser} />
-            </div>
-        </>
+        <div className={classes.wrapper}>
+            <GoBack>Назад</GoBack>
+            <h1 className={classes.title}>Редактирование профиля</h1>
+            <EditProfileForm />
+        </div>
     )
 }
