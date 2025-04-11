92142d1299caf52e3b7d4281d41106ced33d7351
Dewix0 <danbat2006@gmail.com>
2025-04-11 15:29:23 +0300

    added mock data for grenade-class

--- backend/lineups/migrations/migrations.py
+++ backend/lineups/migrations/migrations.py
@@ -26,8 +26,39 @@ def delete_mock_maps(apps, schema_editor):
     Map.objects.all().delete()
 
 
+def create_mock_greande_class(apps, schema_editor):
+    grenade_class = apps.get_model("auth_app", "GrenadeClass")
+
+    grenade_classes = [
+        {
+            "name": "Световая граната",
+            "description": "слепит врага",
+            "price": 200,
+        },
+        {
+            "name": "Осколочная граната",
+            "description": "Наносит урон по области",
+            "price": 300,
+        },
+        {
+            "name": "Дымовая граната",
+            "description": "Создает дымовую завесу в определенном радиусе",
+            "price": 500,
+        },
+    ]
+
+    for gc_data in grenade_classes:
+        grenade_class.objects.create(**gc_data)
+
+
+def delete_mock_grenade_class(apps, schema_editor):
+    gc = apps.get_model("auth_app", "GrenadeClass")
+    gc.objects.all().delete()
+
+
 class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(create_mock_maps, delete_mock_maps),
+        migrations.RunPython(create_mock_greande_class, delete_mock_grenade_class),
     ]
