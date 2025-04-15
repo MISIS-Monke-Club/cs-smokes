d5a730a23b413b8e4bc66afeeec226bbf0b8d322
g0ldexperiencerequiem <pborisenko2006@yandex.ru>
2025-04-15 19:50:17 +0300

    fixed favorites, and swagger

--- backend/favorites/views.py
+++ backend/favorites/views.py
@@ -1,51 +1,51 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
-from auth_app.models import Favorites, Lineup, User
+from auth_app.models import Favorites, Lineup
 from auth_app.serializers import FavoritesSerializer
 
 
-class FavoritesView(APIView):
-    def get_permissions(self):
-        if self.request.method in ["GET"]:
-            return [AllowAny()]
-        return [IsAuthenticated()]
+# class MixedPermissionAPIView(APIView):
+#     def get_permissions(self):
+#         if self.request.method == "GET":
+#             return [AllowAny()]
+#         return [IsAuthenticated()]]
 
-    def get(self, request, userId=None):
-        if userId:
-            user = get_object_or_404(User, id=userId)
-            favorites = Favorites.objects.filter(user=user)
-            serializer = FavoritesSerializer(favorites, many=True)
-            return Response(serializer.data)
-        return Response(
-            {"error": "Не указан user_id"}, status=status.HTTP_400_BAD_REQUEST
-        )
+
+class FavoritesAddView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         grenade_id = request.data.get("grenade_id")
         if not grenade_id:
-            return Response(
-                {"error": "Требуется grenade_id"}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-        lineup = get_object_or_404(Lineup, id=grenade_id)
+            return Response({"error": "Укажи grenade_id"}, status=400)
+        lineup = get_object_or_404(Lineup, grenade_id=grenade_id)
         if Favorites.objects.filter(user=request.user, grenade=lineup).exists():
-            return Response(
-                {"error": "Уже в избранном"}, status=status.HTTP_400_BAD_REQUEST
-            )
-
+            return Response({"error": "Уже в избранном"}, status=400)
         favorite = Favorites.objects.create(user=request.user, grenade=lineup)
         serializer = FavoritesSerializer(favorite)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=201)
 
-    def delete(self, request, grenadeId=None):
-        if not grenadeId:
-            return Response(
-                {"error": "Не указан grenade_id"}, status=status.HTTP_400_BAD_REQUEST
-            )
 
-        favorite = get_object_or_404(Favorites, grenade_id=grenadeId, user=request.user)
+class FavoritesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk=None):
+        if not pk:
+            return Response({"error": "Не указан grenade_id"}, status=400)
+        favorite = get_object_or_404(Favorites, grenade_id=pk, user_id=request.user)
         favorite.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=204)
+
+    def get(self, request, pk=None):
+        if not pk:
+            return Response({"error": "Не указан user_id"}, status=400)
+        favorites = Favorites.objects.filter(user_id=pk)
+        if not favorites.exists():
+            return Response(
+                {"message": "У пользователя нет гранат в избранном", "user_id": pk},
+                status=200,
+            )
+        serializer = FavoritesSerializer(favorites, many=True)
+        return Response(serializer.data, status=200)
