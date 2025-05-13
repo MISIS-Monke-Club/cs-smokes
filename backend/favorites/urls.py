419a2b364e93afc351a74fcf64908e1153b0ea46
Dewi.x0 <danbat2006@gmail.com>
2025-05-13 14:02:07 +0300

    updated urls.py in favorites

--- backend/favorites/urls.py
+++ backend/favorites/urls.py
@@ -2,9 +2,9 @@ from django.urls import re_path
 from .views import FavoritesAddView, FavoritesView
 
 urlpatterns = [
-    re_path(r"^favorites/$", FavoritesAddView.as_view(), name="favorite_add"),
+    re_path(r"^favorites/?$", FavoritesAddView.as_view(), name="favorite_add"),
     re_path(
-        r"^favorites/(?P<pk>\d+)/$",
+        r"^favorites/(?P<pk>\d+)/?$",
         FavoritesView.as_view(),
         name="favorite_get_by_user",
     ),
