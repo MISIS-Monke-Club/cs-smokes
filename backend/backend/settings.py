ae3d7d20332a34f740ce25b8250e06189954deb2
Dewi.x0 <danbat2006@gmail.com>
2025-05-14 20:32:43 +0300

    added channel_layers in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,6 +74,16 @@ CACHES = {
     }
 }
 
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [f"redis://:{os.getenv('REDIS_PASS')}@redis:6379/0"],
+        },
+    },
+}
+
 SIMPLE_JWT = {
     "SIGNING_KEY": SECRET_KEY,
     "USER_ID_FIELD": "user_id",
