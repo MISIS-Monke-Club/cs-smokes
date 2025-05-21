0058aaf7db60832a5ed3bc398e9aea5d7b20c1f9
AlEngerre <semen.lyutoev@yandex.ru>
2025-05-21 18:01:43 +0300

    fixed 404 error

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from pull_requests.routing import websocket_urlpatterns
+from backend.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
