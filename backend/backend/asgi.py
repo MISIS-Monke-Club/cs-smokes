3ef7a2fb016a278d99f46ed563d1d7a9477f89da
AlEngerre <semen.lyutoev@yandex.ru>
2025-05-28 12:09:14 +0300

    black linter adapt

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -11,10 +11,8 @@ import django
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
-
 from backend.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 
